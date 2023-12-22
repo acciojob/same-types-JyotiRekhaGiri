e81af74037eba7434cadf4282df0cc3a5a23e8e3
JyotiRekhaGiri <107474272+JyotiRekhaGiri@users.noreply.github.com>
2023-12-22 16:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function isSameType(value1, value2) {
   //your js code here
-	
-	return typeof value1 === typeof value2;
+	const num1 = parseFloat(value1);
+  const num2 = parseFloat(value2);
+
+  return typeof num1 === 'number' && typeof num2 === 'number';
 }
 // Convert input values to numbers
 
