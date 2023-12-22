1bae99b83396b97e7c9d61e0df3696c3109b89f8
JyotiRekhaGiri <107474272+JyotiRekhaGiri@users.noreply.github.com>
2023-12-22 16:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function isSameType(value1, value2) {
-  //your js code here
-	const num1 = parseFloat(value1);
+  // Convert input values to numbers
+  const num1 = parseFloat(value1);
   const num2 = parseFloat(value2);
 
-  return typeof num1 === 'number' && typeof num2 === 'number';
+  // Check if both values are numbers and have the same type
+  return typeof num1 === 'number' && typeof num2 === 'number' && typeof num1 === typeof num2;
 }
-// Convert input values to numbers
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
