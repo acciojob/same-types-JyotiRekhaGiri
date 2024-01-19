5b4d7fc84f8fc14f9a388eb970400cae9ffc73f0
JyotiRekhaGiri <107474272+JyotiRekhaGiri@users.noreply.github.com>
2024-01-19 10:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
 	
-	if (typeof value1 === typeof value2){
-		return true;
-	}else{
-		return false;
-	}
+	return typeof value1 === typeof value2;
 }
 // Convert input values to numbers
 
@@ -13,3 +9,4 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
